b2b77ef5a000a70f196682ac2eaee41e1e33c64c
ThomasGorisse <thomas.gorisse@gmail.com>
2021-04-09 16:23:12 +0200

    Fixed FullScreen

--- ux/src/main/java/com/google/ar/sceneform/ux/BaseArFragment.java
+++ ux/src/main/java/com/google/ar/sceneform/ux/BaseArFragment.java
@@ -538,7 +538,6 @@ public abstract class BaseArFragment extends Fragment
                                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
