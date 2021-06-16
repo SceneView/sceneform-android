883da230ed3b86b453c4d5a1251467d51587a71c
r.gregat <r.gregat@datalyze-solutions.com>
2021-06-16 08:50:22 +0200

    Copied the gltf sample app to have a separate depth sample app.

---
+++ samples/depth/src/main/java/com/google/ar/sceneform/samples/gltf/MainActivity.java
@@ -0,0 +1,158 @@
+package com.google.ar.sceneform.samples.gltf;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
+
+import com.google.android.filament.ColorGrading;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.CameraStream;
+import com.google.ar.sceneform.rendering.EngineInstance;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.Renderer;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity implements
+        FragmentOnAttachListener,
+        BaseArFragment.OnTapArPlaneListener,
+        ArFragment.OnViewCreatedListener, BaseArFragment.OnSessionConfigurationListener {
+
+    private ArFragment arFragment;
+    private Renderable model;
+    private ViewRenderable viewRenderable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
+
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+        }
+
+        loadModels();
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnTapArPlaneListener(this);
+            arFragment.setOnViewCreatedListener(this);
+            arFragment.setOnSessionConfigurationListener(this);
+        }
+    }
+
+    @Override
+    public void onViewCreated(ArFragment arFragment, ArSceneView arSceneView) {
+        // Currently, the tone-mapping should be changed to FILMIC
+        // because with other tone-mapping operators except LINEAR
+        // the inverseTonemapSRGB function in the materials can produce incorrect results.
+        // The LINEAR tone-mapping cannot be used together with the inverseTonemapSRGB function.
+        Renderer renderer = arSceneView.getRenderer();
+
+        if (renderer != null) {
+            renderer.getFilamentView().setColorGrading(
+                    new ColorGrading.Builder()
+                            .toneMapping(ColorGrading.ToneMapping.FILMIC)
+                            .build(EngineInstance.getEngine().getFilamentEngine())
+            );
+        }
+
+        arSceneView.getCameraStream().setDepthModeUsage(CameraStream.DepthModeUsage.DEPTH_MODE_DISABLED);
+    }
+
+    public void loadModels() {
+        WeakReference<MainActivity> weakActivity = new WeakReference<>(this);
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("https://storage.googleapis.com/ar-answers-in-search-models/static/Tiger/model.glb"))
+                .setIsFilamentGltf(true)
+                .setAsyncLoadEnabled(true)
+                .build()
+                .thenAccept(model -> {
+                    MainActivity activity = weakActivity.get();
+                    if (activity != null) {
+                        activity.model = model;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+        ViewRenderable.builder()
+                .setView(this, R.layout.view_tiger_card)
+                .build()
+                .thenAccept(viewRenderable -> {
+                    MainActivity activity = weakActivity.get();
+                    if (activity != null) {
+                        activity.viewRenderable = viewRenderable;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+    }
+
+    @Override
+    public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+        if (model == null || viewRenderable == null) {
+            Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create the Anchor.
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        // Create the transformable model and add it to the anchor.
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+        model.setParent(anchorNode);
+        model.setRenderable(this.model);
+        model.getRenderableInstance().animate(true).start();
+        model.select();
+
+        Node tigerTitleNode = new Node();
+        tigerTitleNode.setParent(model);
+        tigerTitleNode.setEnabled(false);
+        tigerTitleNode.setLocalPosition(new Vector3(0.0f, 1.0f, 0.0f));
+        tigerTitleNode.setRenderable(viewRenderable);
+        tigerTitleNode.setEnabled(true);
+    }
+
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+        if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC))
+            config.setDepthMode(Config.DepthMode.AUTOMATIC);
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+    }
+}
